77645a3f638f77a20a0aa42055a3b4532c3c25ab
Dan245 <dvmmamo@gmail.com>
2022-03-26 23:55:26 -0400

    Updated for latest questions

--- functions/scrape.py
+++ functions/scrape.py
@@ -20,21 +20,31 @@ def get_screenshots(device):
         time.sleep(0.4)
             
         click_button(browser, "Start school screening")
-            
-        for i in range(2):
-            click_button(browser, "No")
+        
+        click_button(browser, "Yes")
+        click_button(browser, "No")
         
         click_button(browser, "Continue")
         
-        for i in range(4):
+        for i in range(3):
             click_button(browser, "No")
             
 
         browser.find_element_by_tag_name('body').screenshot(f"screenshots/{device['name']}.png")
 
 
-def click_button(browser, text):
-    element = browser.find_element(By.XPATH, f"//*[ text() = '{text}' and @type='button' ]")
+def click_button(browser, text, btType="button"):
+    element = browser.find_element(By.XPATH, f"//*[ text() = '{text}' and @type='{btType}' ]")
     browser.execute_script('arguments[0].click();', element)
     time.sleep(0.4)
     
+def fill_form(browser, id, inpt):
+    element = browser.find_element(By.XPATH, f"//*[ @name='{id}' ]")
+    element.send_keys(inpt)
+    time.sleep(0.4)
+
+def gen_click(browser, id):
+    element = browser.find_element(By.XPATH, f"//*[@id='{id}']")
+    browser.execute_script('arguments[0].click();', element)
+    print("clicked")
+    time.sleep(0.4)
